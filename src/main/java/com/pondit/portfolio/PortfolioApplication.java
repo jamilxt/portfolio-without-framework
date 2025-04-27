c81403ed13abc8c9124213668969398afb808d2e
jamilxt <jamilxt@gmail.com>
2025-04-28 00:47:24 +0600

    config main method to start Embedded Tomcat Server

--- src/main/java/com/pondit/portfolio/PortfolioApplication.java
+++ src/main/java/com/pondit/portfolio/PortfolioApplication.java
@@ -1,7 +1,27 @@
 package com.pondit.portfolio;
 
+import com.pondit.portfolio.controller.RootController;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
 public class PortfolioApplication {
-    public static void main(String[] args) {
-       // TODO: start tomcat server
+    public static void main(String[] args) throws LifecycleException {
+        // prepare the Tomcat server
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        // add the context
+        Context ctx = tomcat.addContext("", null);
+
+        // add the servlet
+        Wrapper servlet = Tomcat.addServlet(ctx, "rootController", new RootController());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*");
+
+        // start the Tomcat server
+        tomcat.start();
     }
 }
